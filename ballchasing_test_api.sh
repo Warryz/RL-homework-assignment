fc8137b5e8ad1eead50579b4c6f7e72ffb243482
Warryz <8317771+Warryz@users.noreply.github.com>
2019-10-16 10:12:59 +0200

    Added curl example

---
+++ ballchasing_test_api.sh
@@ -0,0 +1,7 @@
+curl -v \
+    -H Authorization:abc123 \
+    --url 'https://ballchasing.com/api/replays?playlist=ranked-standard&min-rank=platinum-2&max-rank=platinum-3&count=10' \
+    > beispiel_list.json
+
+
+# Example: https://ballchasing.com/replay/7509cebd-e78e-4214-b92f-024fd39171f5
