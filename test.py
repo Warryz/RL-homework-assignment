8fe73cf27e193b32ed22880366a3de7752c74d45
Warryz <8317771+Warryz@users.noreply.github.com>
2019-10-16 15:58:01 +0200

    Added more code for getting replay data

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import json
+from time import sleep
+
+import requests
 
 with open('beispiel_list.json') as file:
     data = json.load(file)
 
-    replay_url = 'https://ballchasing.com/api/replays/id'
+    replay_url = 'https://ballchasing.com/api/replays/'
     replay_id_dict = dict()
 
     for x in data['list']:
@@ -14,8 +17,20 @@ with open('beispiel_list.json') as file:
 
 
 # Go through the dictionary and download the replay data.
-for x, y in replay_id_dict.items():
-    pass
+for replay_id, json_data in replay_id_dict.items():
+    # Example: https://ballchasing.com/replay/7509cebd-e78e-4214-b92f-024fd39171f5
+
+    # API URL for making requests
+    api_url = f'https://ballchasing.com/api/replay/{replay_id}'
+
+    # Authorization Header
+    header = {'Authorization': '123abc'}
+
+    # Make the first request.
+    r = requests.get(api_url, headers=header)
+
+    # Make the script sleep for 100ms as we're only allowed to do 10 calls per sec
+    time.sleep(0.1)
 
 ranks = [['unranked', 'unranked'],
          ['bronze-1', 'bronze-3'],
