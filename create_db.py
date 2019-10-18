2f371596ff72049b7e8c44e0044a68400f0a60c1
Warryz <8317771+Warryz@users.noreply.github.com>
2019-10-18 21:23:11 +0200

    Added a script to create the database.

---
+++ create_db.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+
+if __name__ == "__main__":
+
+    # Create a database connection and a cursor for executing commands.
+    conn = sqlite3.connect('rl.db')
+    c = conn.cursor()
+
+    # Create database with tables
+    c.executescript('''
+    CREATE TABLE "Players" (
+	"player_id"	INTEGER,
+	"player_name"	TEXT,
+	PRIMARY KEY("player_id"));
+    
+    CREATE TABLE "Replays" (
+	"replay_id"	TEXT,
+	"map"	TEXT,
+	"status"	TEXT,
+	"playlist_id"	TEXT,
+	"duration"	INTEGER,
+	"season"	INTEGER,
+	"min_rank"	TEXT,
+	"max_rank"	TEXT,
+	PRIMARY KEY("replay_id"));
+
+    CREATE TABLE "Stats" (
+	"fk_player_id"	TEXT,
+	"fk_replay_id"	TEXT,
+	"team"	INTEGER,
+	"Stats"	BLOB,
+	FOREIGN KEY("fk_player_id") REFERENCES "Players"("player_id"),
+	PRIMARY KEY("fk_player_id","fk_replay_id"),
+	FOREIGN KEY("fk_replay_id") REFERENCES "Replays"("replay_id"));  
+    ''')
+
+    # close the database connection
+    conn.commit()
+    conn.close()
